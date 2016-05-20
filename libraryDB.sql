a1dba32be6d2d58d7981f61644309b78e99d8691
sameralajooly <sameralajooly@gmail.com>
2016-05-20 19:55:48 +0300

    Add files via upload

--- libraryDB.sql
+++ libraryDB.sql
@@ -35,6 +35,7 @@ CREATE TABLE `authority` (
 
 LOCK TABLES `authority` WRITE;
 /*!40000 ALTER TABLE `authority` DISABLE KEYS */;
+INSERT INTO `authority` VALUES ('abe',1),('samoorking',1);
 /*!40000 ALTER TABLE `authority` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,8 +53,10 @@ CREATE TABLE `book` (
   `state` varchar(45) NOT NULL,
   `author` varchar(45) NOT NULL,
   `employeeID` varchar(16) NOT NULL,
-  PRIMARY KEY (`bookID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`bookID`),
+  KEY `fk_book_employee1_idx` (`employeeID`),
+  CONSTRAINT `fk_book_employee1` FOREIGN KEY (`employeeID`) REFERENCES `employee` (`employeeID`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,6 +65,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Math','Science','1','Abdelsame3','1'),(2,'Physx','Science','0','Abood','2');
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,9 +82,9 @@ CREATE TABLE `client` (
   `DOB` varchar(45) DEFAULT NULL,
   `active` tinyint(4) DEFAULT NULL,
   `capacity` varchar(45) DEFAULT NULL,
-  `numberOfBookesBorrowed` int(11) DEFAULT NULL,
+  `numberOfBooksBorrowed` int(11) DEFAULT NULL,
   PRIMARY KEY (`clientID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,6 +93,7 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (1,'m7md','12/12/2012',1,'3',1);
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,10 +106,12 @@ DROP TABLE IF EXISTS `employee`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employee` (
   `employeeID` varchar(16) NOT NULL,
-  `username` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   `DOB` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`employeeID`)
+  `username` varchar(45) NOT NULL,
+  PRIMARY KEY (`employeeID`),
+  KEY `fk_employee_authority_idx` (`username`),
+  CONSTRAINT `fk_employee_authority` FOREIGN KEY (`username`) REFERENCES `authority` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -114,6 +121,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES ('1','spoon','16/8/1995','samoorking'),('2','spoon','28/2/1995','abe');
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,12 +133,16 @@ DROP TABLE IF EXISTS `reserved`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reserved` (
-  `clientID` int(11) NOT NULL,
   `bookID` int(11) NOT NULL,
-  `employeeID` varchar(16) NOT NULL,
   `fromDate` varchar(45) DEFAULT NULL,
   `toDate` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`clientID`)
+  `employeeID` varchar(16) NOT NULL,
+  `clientID` int(11) NOT NULL,
+  PRIMARY KEY (`bookID`,`clientID`),
+  KEY `fk_reserved_employee1_idx` (`employeeID`),
+  KEY `fk_reserved_client1_idx` (`clientID`),
+  CONSTRAINT `fk_reserved_client1` FOREIGN KEY (`clientID`) REFERENCES `client` (`clientID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reserved_employee1` FOREIGN KEY (`employeeID`) REFERENCES `employee` (`employeeID`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -140,6 +152,7 @@ CREATE TABLE `reserved` (
 
 LOCK TABLES `reserved` WRITE;
 /*!40000 ALTER TABLE `reserved` DISABLE KEYS */;
+INSERT INTO `reserved` VALUES (2,'20/2/2016','4/6/2016','2',1);
 /*!40000 ALTER TABLE `reserved` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,10 +166,12 @@ DROP TABLE IF EXISTS `subscriptions`;
 CREATE TABLE `subscriptions` (
   `fromDate` varchar(45) NOT NULL,
   `toDate` varchar(45) NOT NULL,
-  `clientID` varchar(45) NOT NULL,
   `subscriptionID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`subscriptionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `clientID` int(11) NOT NULL,
+  PRIMARY KEY (`subscriptionID`,`clientID`),
+  KEY `fk_subscriptions_client1_idx` (`clientID`),
+  CONSTRAINT `fk_subscriptions_client1` FOREIGN KEY (`clientID`) REFERENCES `client` (`clientID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -165,6 +180,7 @@ CREATE TABLE `subscriptions` (
 
 LOCK TABLES `subscriptions` WRITE;
 /*!40000 ALTER TABLE `subscriptions` DISABLE KEYS */;
+INSERT INTO `subscriptions` VALUES ('1/1/2011','1/1/2018',1,1);
 /*!40000 ALTER TABLE `subscriptions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -177,4 +193,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-10 14:00:16
+-- Dump completed on 2016-05-20 19:55:21
